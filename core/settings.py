c215559ff7a2238002f0c13b651af131bfdf3b9c
petycinna22-png <pety.cinna.22@gmail.com>
2026-01-14 23:10:43 -0500

    agrega environ

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kn1wy0b^8)alp0&mxjk*i%z19g2m2pe+-8i%zfdpv5-(ojk2#y'
+
+env = environ.Env()
+environ.Env.read_env(env_file=BASE_DIR / '.env')
+
+# SECRET_KEY = 'django-insecure-kn1wy0b^8)alp0&mxjk*i%z19g2m2pe+-8i%zfdpv5-(ojk2#y'
+SECRET_KEY= env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
